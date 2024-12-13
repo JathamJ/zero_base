d9b084e09821f04f9a04bc403658b0e2327cb174
JathamJ <mlqyb@foxmail.com>
2024-12-13 11:39:58 +0800

    feat:增加文件方法、移除gosum

---
+++ constantx/http.go
@@ -0,0 +1,6 @@
+package constantx
+
+const (
+	DefaultUa = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.6"
+	MobileUa  = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/85.0.4183.121"
+)
---
+++ utilx/file.go
@@ -0,0 +1,17 @@
+package utilx
+
+import "os"
+
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	return true
+}
